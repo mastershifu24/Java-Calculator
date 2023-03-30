98970476d0ff92fa31d16e0b4aca76fa77ed4656
Ahmed Shifa <ahmedshifa298@gmail.com>
2023-03-30 10:29:00 -0400

    Add files via upload

---
+++ Java Calculator/src/Calculator.java
@@ -0,0 +1,199 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[9];
+    JButton addButton, subButton, mulButton , divButton;
+    JButton decButton, eqButton, delButton, clrButton, negButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD,30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    String operator;
+
+
+    Calculator(){
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 300, 50);
+        textfield.setFont(myFont);
+        textfield.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        divButton = new JButton("/");
+        mulButton = new JButton("*");
+        eqButton = new JButton("=");
+        decButton = new JButton(".");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+        negButton = new JButton("( - )");
+        
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = divButton;
+        functionButtons[3] = mulButton;
+        functionButtons[4] = eqButton;
+        functionButtons[5] = decButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
+
+        for(int i = 0; i<9; i++){
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for(int i = 0; i<10; i++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+
+        negButton.setBounds(50, 430, 100, 50);
+        delButton.setBounds(150,430,100,50);
+        clrButton.setBounds(250,430,100,50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+        //panel.setBackground(Color.GRAY);
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(eqButton);
+        panel.add(divButton);
+
+
+
+        frame.add(panel);
+        frame.add(negButton);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textfield);
+        frame.setVisible(true);
+
+    }
+public static void main(String[] args){
+   
+    Calculator calc = new Calculator();
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+
+    for(int i = 0; i < 10; i++){
+        if(e.getSource() == numberButtons[i]){
+            textfield.setText(textfield.getText().concat(String.valueOf(i)));
+        }
+    }
+    if(e.getSource() == decButton){
+        textfield.setText(textfield.getText().concat("."));
+    }
+    if(e.getSource() == mulButton){
+        num1 = Double.parseDouble(textfield.getText());
+        operator = "*";
+        textfield.setText("");
+    }
+    if(e.getSource() == subButton){
+        num1 = Double.parseDouble(textfield.getText());
+        operator = "-";
+        textfield.setText("");
+    }
+    if(e.getSource() == addButton){
+        num1 = Double.parseDouble(textfield.getText());
+        operator = "+";
+        textfield.setText("");
+    }
+    if(e.getSource() == divButton){
+        num1 = Double.parseDouble(textfield.getText());
+        operator = "/";
+        textfield.setText("");
+    }
+    if(e.getSource() == eqButton){
+        num2 = Double.parseDouble(textfield.getText());
+        
+        switch(operator) {
+            case "+":
+                result=num1+num2;
+                break;
+        }
+        switch(operator) {
+            case "-":
+                result=num1-num2;
+                break;
+        }
+        switch(operator) {
+            case "*":
+                result=num1*num2;
+                break;
+        }
+        switch(operator) {
+            case "/":
+                result=num1/num2;
+                break;
+        }
+        textfield.setText(String.valueOf(result));
+        num1=result;
+    }
+    if(e.getSource() == clrButton){
+        textfield.setText("");
+    }
+    if(e.getSource() == delButton){
+        String string = textfield.getText();
+        textfield.setText("");
+        for(int i = 0; i < string.length() - 1; i++){
+            textfield.setText(textfield.getText()+string.charAt(i));
+
+        }
+    }
+    if(e.getSource() == negButton){
+        double temp = Double.parseDouble(textfield.getText());
+        temp*=-1;
+        textfield.setText(String.valueOf(temp));
+
+        }
+    }
+}
+    
+// Create a Java Calculator Class that does the following five commands:
+// An add method that takes in two integer numbers and adds them together the call to that method would look like this: add(int num1, int num2)
+// A subtraction method that takes in two integers and subtracts them from one another would look like this: subtract( int num1, int num2)
+// A multiplication method that takes in two integer numbers and multiplies them together the call to that method would look like this: multiply(int num1, int num2)
+// A division method that takes in two integer numbers and divides them the call to that method would look like this: divide(int num1, int num2)
+// A square method that takes in one integer and squares it: square(int num1, int num2)
+
+// Create a MagicCalculator that inherits its basic functionality from your calculator and does the following functions:
+// Finds the square root of a number
+// Finds the sin (trigonometry) of a number. [Note: Feel free to use the MATH class]
+// Finds the cosine (trigonometry) of a number. [Note: Feel free to use the MATH class]
+// Finds the tangent (trigonometry) of a number. [Note: Feel free to use the MATH class]
+// Finds the factorial (!) of the number
+
